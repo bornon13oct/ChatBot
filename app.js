9e4f42c192dac2d08545c71166c73c8a2a13fba6
bornon13oct <bornon13oct@gmail.com>
2018-03-15 17:11:16 +0000

    Initiating Chatbot

---
+++ app.js
@@ -0,0 +1,23 @@
+var express    = require("express"),
+    bodyParser = require("body-parser"),
+    request    = require("request"),
+    app        = express();
+    
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get("/", function(req, res){
+    res.send("HI I'm a chatbot");
+});
+
+app.get("/webhook", function(req, res){
+    if(req.query["hub.verify_token"] === "skay13oct"){
+        res.send(req.query["hub.challenge"]);
+    }
+    res.send("Wrong token");
+});
+    
+app.listen(process.env.PORT, process.env.IP, function(){
+console.log("server has started.");
+});
+
